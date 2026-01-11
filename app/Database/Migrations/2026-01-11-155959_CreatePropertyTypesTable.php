deeba12967030e802ad1d7e81d1f7f190dfac54f
teljaoui <teljaoui2004@gmail.com>
2026-01-11 17:51:07 +0100

    up

--- app/Database/Migrations/2026-01-11-155959_CreatePropertyTypesTable.php
+++ app/Database/Migrations/2026-01-11-155959_CreatePropertyTypesTable.php
@@ -11,6 +11,8 @@ class CreatePropertyTypesTable extends Migration
         $this->forge->addField([
             'id' => ['type' => 'INT', 'auto_increment' => true],
             'name' => ['type' => 'VARCHAR', 'constraint' => 50],
+            'created_at' => ['type' => 'DATETIME', 'null' => true],
+            'updated_at' => ['type' => 'DATETIME', 'null' => true],
         ]);
         $this->forge->addKey('id', true);
         $this->forge->createTable('property_types');
